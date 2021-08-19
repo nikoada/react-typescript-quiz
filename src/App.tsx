6bdc2d52e36f73c60c91dc6f05f0bda339479432
nikoada <nikushac@gmail.com>
2021-08-19 16:50:14 +0200

    feat: add conditional rendering to UI

--- src/App.tsx
+++ src/App.tsx
@@ -48,19 +48,23 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
       <button className="start" onClick={startTrivia}>
         Start
       </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions ...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnser={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
+      ) : null }
+      { !gameOver ? <p className="score">Score:</p> : null }
+      {loading && <p>Loading Questions ...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnser={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
