6589fe1c379005b2ee218080108e3974a7842c5f
nikoada <nikushac@gmail.com>
2021-09-05 15:25:02 +0200

    feat: add isNumber prop to Select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { EnumType } from 'typescript';
 import { Caterogies, Difficulty, TotalAmount } from '../API';
 
 type Props = {
     title: string;
     enumName: typeof Caterogies | typeof Difficulty | typeof TotalAmount;
     setOption: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    isNumber: boolean;
 }
 
-const Select: React.FC<Props> = ({ title, enumName, setOption }) => (
+const Select: React.FC<Props> = ({ title, enumName, setOption, isNumber }) => (
     <>
     <label>{title}</label>
         <select name={title.toLowerCase()} onChange={setOption}>
           {Object.keys(enumName).map((item, index) => {
-            return isNaN(Number(item)) && <option value={item} key={index}>{item}</option>
+            return isNumber ? !isNaN(Number(item)) && <option value={item} key={index}>{item}</option>
+            : isNaN(Number(item)) && <option value={item} key={index}>{item}</option>
           })}
         </select>
     </>
